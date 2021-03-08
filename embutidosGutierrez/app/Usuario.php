1fec53cb7af697148a22b77b9a0d16330688c02f
Javi <javi17pm@gmail.com>
2021-03-08 11:35:34 +0100

    usuario.php actualizado

--- embutidosGutierrez/app/Usuario.php
+++ embutidosGutierrez/app/Usuario.php
@@ -3,8 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Pedido;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
     use Notifiable;
 
