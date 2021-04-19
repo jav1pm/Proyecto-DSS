b71f8911606721b419a9541c1680158cc6277f4d
fmurciag <fmurciag@gmail.com>
2021-04-19 10:30:24 +0200

    pseudoMenu

--- embutidosGutierrez/resources/views/orderlines/orderlinesShow.blade.php
+++ embutidosGutierrez/resources/views/orderlines/orderlinesShow.blade.php
@@ -7,9 +7,42 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 </head>
-<body>
+<style>
+   ul{
+      margin:0;
+      padding:0;
+      box-sizing:border-box;
+      font-size:0;
+      }
+            
+      nav a{
+         text-decoration:none;
+         color:inherit;
+         font-size:14px;
+      } 
+      nav li{
+         display:inline-block;
+         width:10%;
+         padding:5px 10px;
+         background-color:#000;
+         border:1px solid #000;
+         text-align:center;
+         color:#fff;
+      }
+      nav li:hover{
+         background-color:#fff;
+         color:#000;
+      }
+</style>
 <body>
 <center>
+<nav><ul>
+        <li><a href="{{ route('products.showProducts')}}">Productos</a></li>
+        <li><a href="{{ route('users.showAll')}}">Usuarios</a></li>
+        <li><a href="{{ route('orders.showOrders')}}">Pedidos</a></li>
+        <li><a href="localhost:8000/orderlines">Lineas de pedido</a></li>
+        <li><a href="{{ route('categories.showCategories')}}">Categorias</a></li>
+    </ul></nav>
       <h3> Linea de pedido </h3>
    <div class="container">
       <table class="table table-striped" border=“6px”; style="text-align:center">
